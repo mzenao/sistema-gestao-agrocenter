2f194ca62f678f03cc43249ddbe0b7787d43dc4f
Mzenao <mzenao@hotmail.com>
2026-02-02 15:06:41 -0300

    Ajustes hora automática vendas

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
-from SQLAlchemy import DateTime
 from datetime import datetime, date
 from collections import defaultdict
 from collections import OrderedDict
